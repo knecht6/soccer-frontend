05bfcc75034be0e6d8b8187b24cf33f72054bf3b
Student <palaciosdiego759@gmail.com>
2020-08-07 17:20:52 +0200

    I'm not remember

--- src/components/Odometer.jsx
+++ src/components/Odometer.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function Odometer({ value }) {
   const [counter, setCounter] = useState(0);
   useEffect(() => {
-    if (counter < value) setTimeout(() => setCounter(counter + 1), 15);
+    if (counter < value) setTimeout(() => setCounter(counter + 1), 30);
   }, [counter, value]);
   return <div className="odometer">{counter}</div>;
 }
